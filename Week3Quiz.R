aa8b1a53e66923ce54ca9edd357805f0c2e77651
Christopher Krolak <cjkrolak@gmail.com>
2021-02-16 09:14:59 -0600

    question3 complete

--- Week3Quiz.R
+++ Week3Quiz.R
@@ -16,3 +16,15 @@ Q2Jpeg <- function() {
     img.n <- readJPEG(tempfile, native = TRUE)
     q <- quantile(img.n, probs=c(0.30, 0.80))  # quantiles
 }
+
+Q3Match <- function() {
+    url1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+    url2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+    gdp <- read.csv(url1)
+    edu <- read.csv(url2)
+    mergedData <- merge(gdp, edu, by.x="X", by.y="CountryCode", all=FALSE)
+    mergedData <- mutate(mergedData, rank=as.numeric(Gross.domestic.product.2012))  # rank numeric column
+    mergedData <- arrange(mergedData[!is.na(mergedData$rank),], desc(rank))  # remove NA and sort desc
+    print(paste("number of matches=", length(mergedData$rank)))
+    result <- mergedData[13, "Short.Name"]
+}
